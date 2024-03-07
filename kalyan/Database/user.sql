2d0d5b934faeb9abb6e2b6ddda9ec0b26906336c
venkatkalyan12 <155452623+venkatkalyan12@users.noreply.github.com>
2024-03-07 09:50:38 +0530

    Update user.sql

--- kalyan/Database/user.sql
+++ kalyan/Database/user.sql
@@ -1,9 +1,9 @@
 CREATE TABLE `users` (
   User_id INT NOT NULL,
-  `Firstt_Name` VARCHAR(45) NULL,
-  `Last_Name` VARCHAR(45) NULL,
+  `Firstt_Name` VARCHAR(45) NOT NULL,
+  `Last_Name` VARCHAR(45) NOT NULL,
   `Date_Of_Birth` DATE NULL,
-  `Phone_Number` int(12) NULL,
+  `Phone_Number` int(12) NOT NULL,
   `Address` VARCHAR(45) NULL,
-  `PassWord` CHAR(20) NULL,
+  `PassWord` CHAR(20) NOT NULL,
   PRIMARY KEY (`User_id`));
