3aaf7b097353e0b2cd0a43a2e141b59f8eb99750
venkatkalyan12 <155452623+venkatkalyan12@users.noreply.github.com>
2024-03-08 07:01:13 +0530

    Delete kalyan/Database directory

--- kalyan/Database/Wallet.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `wallet` (
-  `User_id` int NOT NULL,
-  `Current_Bal` int DEFAULT NULL,
-  KEY `Current_Bal_idx` (`User_id`),
-  CONSTRAINT `Current_Bal` FOREIGN KEY (`User_id`) REFERENCES `users` (`User_id`)
-)
--- kalyan/Database/user.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `users` (
-  User_id INT NOT NULL,
-  `Firstt_Name` VARCHAR(45) NOT NULL,
-  `Last_Name` VARCHAR(45) NOT NULL,
-  `Date_Of_Birth` DATE NULL,
-  `Phone_Number` int(12) NOT NULL,
-  `Address` VARCHAR(45) NULL,
-  `PassWord` CHAR(20) NOT NULL,
-  PRIMARY KEY (`User_id`));
